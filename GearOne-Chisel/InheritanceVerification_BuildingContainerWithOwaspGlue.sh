070c86d47bfc4bc3b9ba7df9bee94150c5fb4bfc
Ayman Kamran <aymanbinkamran@gmail.com>
2020-11-10 23:11:57 +0400

    GearOne: added prerequisites

--- GearOne-Chisel/InheritanceVerification_BuildingContainerWithOwaspGlue.sh
+++ GearOne-Chisel/InheritanceVerification_BuildingContainerWithOwaspGlue.sh
@@ -1,1 +1,14 @@
 # Report Developer - Recipient: Auditors
+docker run -v "${PWD}":/src/ --rm owasp/glue –t OWASPDependencyCheck -d /src
+
+# Jira Enabler - ChatOps & Webhooks
+docker run -v "${PWD}":/src/ --rm owasp/glue -t OWASPDependencyCheck -d /src -f jira –jira-api-url https:/jira.example.com –jira-username “my_jira_user” –jira-password “XXXX” –jira-project MYPROJ
+
+# OWASP supports Java and .NET, but there is experimental support Ruby, Node.js, Python, and in addition to limited support for C/C++.
+# Personal Experimenter
+docker run –v your_source_code_path:/src –rm owasp/glue –t OWASPDependencyCheck –d /src
+
+#prerequisites:
+# docker installation
+# place this file in the directory of source code
+# replace /src/ with file path of directory under question 
