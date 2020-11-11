eec32bd5f1e186c5588654b9e54e804aca77d9c8
Ayman Kamran <aymanbinkamran@gmail.com>
2020-11-12 03:55:01 +0400

    added ZapSpiderAlerts and GoogleGruyereInspections

--- GearOne-Chisel/InheritanceVerification_BuildingContainerWithOwaspGlue.sh
+++ GearOne-Chisel/InheritanceVerification_BuildingContainerWithOwaspGlue.sh
@@ -1,13 +1,12 @@
 # Report Developer - Recipient: Auditors
-docker run -v "${PWD}":/Users/dapicoop/Desktop/SideProjects/AXLE-DevSecOps-ToolChain/AXLE-DevSecOps-ToolChain/GearOne-Chisel/InheritanceVerification_BuildingContainerWithOwaspGlue.sh --rm owasp/glue –t OWASPDependencyCheck -d /src
-# docker run -v "${PWD}":/src/ --rm owasp/glue –t OWASPDependencyCheck -d /src
+docker run -v "${PWD}":/src/ --rm owasp/glue –t OWASPDependencyCheck -d /src
 
 # # Jira Enabler - ChatOps & Webhooks
-# docker run -v "${PWD}":/src/ --rm owasp/glue -t OWASPDependencyCheck -d /src -f jira –jira-api-url https:/jira.example.com –jira-username “my_jira_user” –jira-password “XXXX” –jira-project MYPROJ
+docker run -v "${PWD}":/src/ --rm owasp/glue -t OWASPDependencyCheck -d /src -f jira –jira-api-url https:/jira.example.com –jira-username “my_jira_user” –jira-password “XXXX” –jira-project MYPROJ
 
 # # OWASP supports Java and .NET, but there is experimental support Ruby, Node.js, Python, and in addition to limited support for C/C++.
 # # Personal Experimenter
-# docker run –v your_source_code_path:/src –rm owasp/glue –t OWASPDependencyCheck –d /src
+docker run –v your_source_code_path:/src –rm owasp/glue –t OWASPDependencyCheck –d /src
 
 #prerequisites:
 # docker installation
